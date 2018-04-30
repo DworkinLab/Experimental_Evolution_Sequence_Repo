2c93ff0c9191b77a144bb4918e418249b36923d1
PaulKnoops <PaulKnoops@users.noreply.github.com>
2018-04-30 10:09:41 -0400

    Update model_p.adjustFDR.R

--- Analysis_after_BAM_Scripts/model_p.adjustFDR.R
+++ Analysis_after_BAM_Scripts/model_p.adjustFDR.R
@@ -43,3 +43,29 @@ theme(text = element_text(size=20), axis.text.x= element_text(size=15), axis.tex
 ylab("-log10(p.adjust)")
 
 #print(gdat)
+
+
+CHROMOs_4  <- fread('../Data/Model_Chromo_FDRp.adjust.csv')
+#CHROMOs_4  <- fread('../Data/Model_Chromo_Bonfp.adjust.csv')
+Zxc_length <- fread('../Data/CHROMO_model_Full_LENGTH.csv')
+
+a <- Zxc_length$x[1]
+b <- Zxc_length$x[2]
+c <- Zxc_length$x[3]
+d <- Zxc_length$x[4]
+e <- Zxc_length$x[5]
+f <- Zxc_length$x[6]
+
+gdat <- ggplot(data = CHROMOs_4, aes(x=number, y=logAdjustP, colour = chr)) + 
+geom_point(size = 0.5, show.legend = F) + 
+theme(panel.background = element_blank()) + 
+xlab("Chromosome") + 
+scale_x_discrete(limits=c(a/2, a+(b/2), (a+b+ (c/2)), (a+b+c+(d/2)), (a+b+c+d+(e/2)), (a+b+c+d+e+(f/2))), 
+                 labels = c("X", "2L", '2R', '3L', "3R", "4")) + 
+scale_colour_manual(values=c("#56B4E9", "#E69F00", 'grey30', 'grey46', 'wheat3', 'lemonchiffon4')) + 
+theme(text = element_text(size=20), axis.text.x= element_text(size=15), 
+      axis.text.y= element_text(size=15)) + 
+ylab("-log10(p.adjust)")
+
+print(gdat)
+
